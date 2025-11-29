b4db84dc86756a6c52c0f2c4490824b304b39c2e
Andy-Ops254 <ghostandy254@outlook.com>
2025-11-29 03:18:12 +0300

    Add relationships on models

--- server/models.py
+++ server/models.py
@@ -12,6 +12,10 @@ class User(db.Model, SerializerMixin):
     password_hash = db.Column(db.String)
     created_at =db.Column(db.DateTime, server_default=db.func.now())
 
+    emotion_logs=db.relationship('Emotion_log', back_populates='user')
+    reading_logs=db.relationship('Reading_log',back_populates='user')
+
+
     def __repr__(self):
         return f'<User email{self.email}, created_at{self.email}>'
 
@@ -22,6 +26,9 @@ class Daily_reading (db.model, SerializerMixin):
     id = db.Column (db.Integer, primary_key=True)
     date = db.Column(db.DateTime, server_default=db.func.now())
     scripture = db.Column(db.Text)
+
+    reading_logs= db.relationship('Reading_logs', back_populates='daily_reading', cascade='all, delete-orphan')
+
 
     def __repr__(self):
         return f'<Daily_readings date={self.date}, scripture={self.scripture}>'
@@ -36,6 +43,9 @@ class Reading_logs(db.model, SerializerMixin):
     completed = db.Column(db.Boolean, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
+    user=db.relationship('User', back_populates='reading_logs')
+    daily_reading = db.relationship('Daily_reading', back_populates='reading_logs', cascade='all, delete-orphan')
+
     def __repr__(self):
         return f'<Reading_logs user_id={self.user_id},daily_readings_id={self.daily_readings_id}, completed={self.completed}, created_at= {self.created_at}>'
 
@@ -48,6 +58,9 @@ class Emotion_logs(db.Model, SerializerMixin):
     emotion_text=db.Column(db.Text, nullable=False)
     scripture = db.Column(db.Text)
     time = db.Column(db.DateTime, server_default=db.func.now())
+
+    user=db.relationship('User', back_populates='emotion_logs')
+
 
     def __repr__(self):
         return f'<Emotion_log users_id={self.users_id}, emotion_text = {self.emotion_text}, scripture={self.scripture}, time={self.time}>'
