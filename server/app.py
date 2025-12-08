d18d3217e59982f7d3228923403a687694b54918
Andy-Ops254 <ghostandy254@outlook.com>
2025-12-09 00:21:52 +0300

    complete login, logoutand register routes

--- server/app.py
+++ server/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request,jsonify
+from flask import Flask, request,jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
 from extension import db, bcrypt
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_jwt_extended import (
-    create_access_token, jwt_required, get_jwt_identity
+    create_access_token, jwt_required, get_jwt_identity, JWTManager, get_jwt
 )
 from datetime import datetime, timedelta
 
@@ -13,30 +13,36 @@ from datetime import datetime, timedelta
 
 
 migrate = Migrate()
+jwt = JWTManager()
+blacklist = set()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     bcrypt.init_app(app)
-
     db.init_app(app)
-    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_revoked(jwt_header, jwt_payload):
+        # return True if token has been revoked (jti is in blacklist)
+        return jwt_payload.get("jti") in blacklist
 
 
     # Import models here so Flask-Migrate sees them
     from models import User, Daily_reading, Emotion_logs, Reading_logs
 
-    @app.route('/login', METHODS=['POST'])
+    @app.route('/login', methods=['POST'])
     def app_login ():
         data = request.get_json()
         # handles the error that may result from blank spaces from the users request
-        if not data or"email" in data or "password" in data:
+        if not data or"email" not in data or "password" not in data:
             return jsonify({'error': 'Email and password are required!'}), 400
         
         # get password and email from the clients request
-        email = data['email'].strip().lower()
-        password = data['password']
+        email = data["email"].strip().lower()
+        password = data["password"]
 
         # Validate user
         user = User.query.filter_by(email=email).first()
@@ -44,7 +50,7 @@ def create_app():
             return jsonify({"error": "Unknown User!"}), 401
         
         # valid passowrd
-        if not check_password_hash(user.password, password):
+        if not bcrypt.check_password_hash(user.password_hash, password):
             return jsonify({"error": "Invalid Password!"}), 401
         
         # Generate the tokens
@@ -56,11 +62,48 @@ def create_app():
             "message": "Login Successful",
             "access_token": access_token,
             "user": {
-                id: "user.id",
-                email: "user.email"
+                "id": user.id,
+                "email": user.email
             }
         }), 200
+
+
+    @app.route('/register', methods=['POST'])
+    def user_registration():
+        data = request.get_json()
+
+        email = data.get("email").strip().lower()
+        if not email:
+            return{"error":"Email is required"}, 400
+        
+        raw_password = data.get("password")
+        if not raw_password:
+            return {"error": "Password is required"}, 400
+        
+        # check if the user exists in the database:
+        existing_user= User.query.filter_by(email=email).first()
+        if existing_user:
+            return {"error": "Email is in use!"}, 409
+        else:
+            new_user = User(
+                email= email,
+                password= raw_password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            user_dict =new_user.to_dict(only=('id', 'email'))
+            return make_response(user_dict, 201)
+
+        
+    @app.route('/logout', methods=['DELETE'])
+    @jwt_required()
+    def logout():
+        jti = get_jwt()["jti"] #gets the token id
+        blacklist.add(jti) #adds token to blocklist
+        return jsonify({"message": "Successfully logged out"}), 200
+        
     
+        
     return app
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -17,8 +17,8 @@ class User(db.Model, SerializerMixin):
     password_hash = db.Column(db.String)
     created_at =db.Column(db.DateTime, server_default=db.func.now())
 
-    emotion_logs=db.relationship('Emotion_log', back_populates='user')
-    reading_logs=db.relationship('Reading_log',back_populates='user')
+    emotion_logs=db.relationship('Emotion_logs', back_populates='user')
+    reading_logs=db.relationship('Reading_logs',back_populates='user')
 
     @validates ('email')
     def validate_email (self, key, email):
@@ -26,26 +26,20 @@ class User(db.Model, SerializerMixin):
             raise TypeError('Invalid email format!')
         else:
             return email
-    
-    @validates ('password')
-    def validate_password(self, key, password):
-        if len(password) < 8 :
-            raise ValueError("Password must be 8 character long !")
-        else:
-            return password
 
     @property
     def password(self):
         raise AttributeError("Password is not readable!")
     
     @password.setter
-    def set_password(self, raw_password):
+    def password(self, raw_password):
+        if len(raw_password) < 8 :
+            raise ValueError("Password must be 8 character long !")
         # validate is triggered here
-        self.validate_password("password", raw_password)
         password_hash = bcrypt.generate_password_hash(
                 raw_password.encode('utf-8'))
         # after validation generate the password hash
-        self.password_harsh =password_hash.decode('uft-8')
+        self.password_hash =password_hash.decode('utf-8')
 
 
     def __repr__(self):
@@ -59,7 +53,7 @@ class Daily_reading (db.Model, SerializerMixin):
     date = db.Column(db.DateTime, server_default=db.func.now())
     scripture = db.Column(db.Text)
 
-    reading_logs= db.relationship('Reading_logs', back_populates='daily_reading', cascade='all, delete-orphan')
+    reading_logs= db.relationship('Reading_logs', back_populates='daily_reading')
 
 
     def __repr__(self):
@@ -76,7 +70,7 @@ class Reading_logs(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
     user=db.relationship('User', back_populates='reading_logs')
-    daily_reading = db.relationship('Daily_reading', back_populates='reading_logs', cascade='all, delete-orphan')
+    daily_reading = db.relationship('Daily_reading', back_populates='reading_logs')
 
     def __repr__(self):
         return f'<Reading_logs user_id={self.user_id},daily_readings_id={self.daily_readings_id}, completed={self.completed}, created_at= {self.created_at}>'
